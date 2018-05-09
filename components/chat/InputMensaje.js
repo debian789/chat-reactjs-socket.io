c0d87c5718a613ec1ffe66ac31d2945bab26c00b
Miguel Suescun <debian789@gmail.com>
2018-05-09 10:04:52 -0500

    Update InputMensaje.js

--- components/chat/InputMensaje.js
+++ components/chat/InputMensaje.js
@@ -1,26 +1,20 @@
 import React from 'react';
 
-
 export default class InputMensaje extends React.Component{
-
 	onClick(evento){
 		//13 Corresponde a la tecla Enter
 		if(evento.keyCode == 13){
 			this.props.onSendMensaje.call(null,{mensaje:evento.target.value})
 			evento.target.value="";
 		}
-
 	}
 
 	render(){
 		return <div className="inputChat">
-			
 			<input 
 			type="text" 			
 			placeholder="Escribe un mensaje y preciona la tecla enter " 				
 			onKeyDown={this.onClick.bind(this)}/>
-			
-
 		</div>
 	}
 }
