39424a017a73ec8e8a73bf9eaf64db99434aae1e
Miguel Suescun <debian789@gmail.com>
2018-05-09 09:40:57 -0500

    Update ItemListChat.js

--- components/chat/ItemListChat.js
+++ components/chat/ItemListChat.js
@@ -2,13 +2,15 @@ import React from 'react';
 
 export default class ItemListChat extends React.Component{
 	render(){
+		// Con this.props.estiloItem optenemos los estilos CSS 
+		// que permiten distinguir si es un mensaje entrate o saliente 
+		// this.props.user Obtener el nombre del usuario que envio el mensaje 
+		// this.props.mensaje Obtener el mensaje que se envio
 		return 	<div className={this.props.estiloItem} >
 				<span>{this.props.user}</span>
 				<p>{this.props.mensaje}</p>
 			</div>
 	}
 }
-
-
 
 ItemListChat.defaultProps = {estiloItem:"itemListChat2"}
